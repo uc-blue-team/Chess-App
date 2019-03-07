ea837ac5425ee579dba0c7a094c6e1cfa2ddb039
Jonathan <jjstone707@gmail.com>
2019-03-07 01:54:10 +0000

    added devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'https://chess-app-uc-blue-team.herokuapp.com/' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
