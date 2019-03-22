152630996c1fdb989d836d8b184b1c9a15ccb371
Jonathan <jjstone707@gmail.com>
2019-03-22 22:36:15 +0000

    added many more tests and bug fixes

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -2,10 +2,8 @@ class Piece < ApplicationRecord
 	belongs_to :game
 
 	def lat_check(dest_x,dest_y)
-		start_x = x_position
-		start_y = y_position
 		game.pieces.each do |piece|
-			if piece.y_position == start_y && piece.x_position.between?(start_x, dest_x) && piece.x_position != start_x
+			if piece.y_position == y_position && (piece.x_position.between?(dest_x, x_position) or piece.x_position.between?(x_position, dest_x)) && piece.x_position != x_position
 				return true
 			end
 		end
@@ -13,10 +11,8 @@ class Piece < ApplicationRecord
 	end
 
 	def long_check(dest_x,dest_y)
-		start_x = x_position
-		start_y = y_position
 		game.pieces.each do |piece|
-			if piece.x_position == start_x && piece.y_position.between?(start_y, dest_y) && piece.y_position != start_y
+			if piece.x_position == x_position && (piece.y_position.between?(dest_y, y_position) or piece.y_position.between?(y_position, dest_y)) && piece.y_position != y_position
 				return true
 			end
 		end
@@ -24,14 +20,12 @@ class Piece < ApplicationRecord
 	end
 
 	def diag_check(dest_x,dest_y)
-		start_x = x_position
-		start_y = y_position
-		movement = (start_x - dest_x).abs
-		directionX = (dest_x - start_x)/(dest_x - start_x).abs
-		directionY = (dest_y - start_y)/(dest_y - start_y).abs
+		movement = (x_position - dest_x).abs
+		directionX = (dest_x - x_position)/(dest_x - x_position).abs
+		directionY = (dest_y - y_position)/(dest_y - y_position).abs
 		(1..movement).each do |i|
 			game.pieces.each do |piece|
-				if start_x + i * (directionX) == piece.x_position && start_y + i * (directionY) == piece.y_position
+				if x_position + i * (directionX) == piece.x_position && y_position + i * (directionY) == piece.y_position
 					return true
 				end
 			end
@@ -40,15 +34,13 @@ class Piece < ApplicationRecord
 	end
 
 	def is_obstructed?(dest_x,dest_y)
-		start_x = x_position
-		start_y = y_position
-		if dest_x > 7 || dest_y > 7
+		if dest_x > 7 || dest_y > 7 || dest_x < 0 || dest_y < 0 
 			raise "invalid input"
-		elsif start_y == dest_y
+		elsif y_position == dest_y
 			return lat_check(dest_x,dest_y)
-		elsif start_x == dest_x
+		elsif x_position == dest_x
 			return long_check(dest_x,dest_y)
-		elsif ((start_y - dest_y)/(start_x - dest_x)).abs.eql?(1)
+		elsif ((y_position - dest_y)/(x_position - dest_x)).abs.eql?(1)
 			return diag_check(dest_x,dest_y)
 		else
 			raise "invalid input"
