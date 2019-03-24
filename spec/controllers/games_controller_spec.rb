04fd04f2f80675e7251bde8056982c6f926a232f
Kevins MBP <kevlau68@gmail.com>
2019-03-24 00:33:40 +0000

    added tests to the model as well as a test gem file

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
+	it 'returns :new view' do
+		get :new
+		expect(response).to render_template('new')
+	end
 
+	it 'assigns :name to kevin' do
+		get :new
+		expect(assigns(:name)).to eq 'kevin'
+	end
 end
