d4fbb06efd9b33f189cb185a1c8ce4e3efa26f59
NineDrop <JJStone707@gmail.com>
2019-03-24 21:20:33 -0700

    Update game.rb
    also destroyed my tests

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,3 @@
 class Game < ApplicationRecord
-	belongs_to :white_player, :class_name => "User", :foreign_key => "WhitePlayer"
-	belongs_to :black_player, :class_name => "User", :foreign_key => "BlackPlayer"
 	has_many :pieces
 end
