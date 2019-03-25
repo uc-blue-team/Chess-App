4d6df8b70537a88ed8e8c2ddb1540b2a2d6b84d4
Kevins MBP <kevlau68@gmail.com>
2019-03-25 09:05:47 +0000

    corrected model to create correct number of pawns

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,4 @@
 class Game < ApplicationRecord
-	belongs_to :white_player, :class_name => "User", :foreign_key => "WhitePlayer"
-	belongs_to :black_player, :class_name => "User", :foreign_key => "BlackPlayer"
 	has_many :pieces
 
 
@@ -8,31 +6,31 @@ class Game < ApplicationRecord
 	after_create :populate_board!
 
 	def populate_board!
-		[0..7].each do |x_pos|
-			Pawn.create(game: self, player: self.white_player, x_position: x_pos, y_position: 1)
-			Pawn.create(game: self, player: self.black_player, x_position: x_pos, y_position: 6)
+		(0..7).each do |x_pos|
+			Pawn.create(game: self, color: white, x_position: x_pos, y_position: 1)
+			Pawn.create(game: self, color: black, x_position: x_pos, y_position: 6)
 		end
 
 		[0,7].each do |x_pos|
-			Rook.create(game: self,player: self.white_player, x_position: x_pos, y_position: 0)
-			Rook.create(game: self,player: self.black_player, x_position: x_pos, y_position: 7)
+			Rook.create(game: self, color: white, x_position: x_pos, y_position: 0)
+			Rook.create(game: self, color: black, x_position: x_pos, y_position: 7)
 		end
 
 		[1,6].each do |x_pos|
-			Knight.create(game: self,player: self.white_player, x_position: x_pos, y_position: 0)
-			Knight.create(game: self,player: self.black_player, x_position: x_pos, y_position: 7)
+			Knight.create(game: self, color: white, x_position: x_pos, y_position: 0)
+			Knight.create(game: self, color: black, x_position: x_pos, y_position: 7)
 		end
 
 		[2,5].each do |x_pos|
-			Bishop.create(game: self,player: self.white_player, x_position: x_pos, y_position: 0)
-			Bishop.create(game: self,player: self.black_player, x_position: x_pos, y_position: 7)
+			Bishop.create(game: self, color: white, x_position: x_pos, y_position: 0)
+			Bishop.create(game: self, color: black, x_position: x_pos, y_position: 7)
 		end
 
-		Queen.create(game: self,player: self.white_player, x_position: 4, y_position: 0)
-		Queen.create(game: self,player: self.black_player, x_position: 4, y_position: 7)
+		Queen.create(game: self, color: white, x_position: 3, y_position: 0)
+		Queen.create(game: self, color: black, x_position: 3, y_position: 7)
 
-		King.create(game: self,player: self.white_player, x_position: 3, y_position: 0)
-		King.create(game: self,player: self.black_player, x_position: 3, y_position: 7)
+		King.create(game: self, color: white, x_position: 4, y_position: 0)
+		King.create(game: self, color: black, x_position: 4, y_position: 7)
 
 	end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,5 @@
 class User < ApplicationRecord	
 	has_many :games
-	has_many :white_games, :class_name => "Games", :foreign_key => "WhitePlayer"
-	has_many :black_games, :class_name => "Games", :foreign_key => "BlackPlayer"
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
