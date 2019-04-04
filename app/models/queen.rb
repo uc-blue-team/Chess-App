82aca4b36c6bacdd7220489a0c64cd1c12fce1e2
Jonathan <jjstone707@gmail.com>
2019-04-04 23:20:26 +0000

    fixed some syntax

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -3,7 +3,7 @@ class Queen < Piece
 	def valid_move?(dest_x, dest_y)
 		if off_the_board?(dest_x,dest_y)
 			raise "invalid input"
-		elsif (is_lateral?(dest_x,dest_y) or is_vertical?(dest_x,dest_y or is_diagonal?(dest_x,dest_y)) && !is_obstructed(dest_x, dest_y) && !is_occupied_by_friend?(dest_x, dest_y) 
+		elsif (is_lateral?(dest_x,dest_y) || is_vertical?(dest_x,dest_y || is_diagonal?(dest_x,dest_y)) && !is_obstructed(dest_x, dest_y) && !is_occupied_by_friend?(dest_x, dest_y) 
 			return true
 		else
 			raise "invalid input"
