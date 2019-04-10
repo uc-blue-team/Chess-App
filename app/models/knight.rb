5433642f920ab88dd1bd737b9c5aa80ee5329f8b
Kevins MBP <kevlau68@gmail.com>
2019-04-10 20:54:57 +0000

    added knight valid move

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,6 +1,18 @@
 class Knight < Piece
 
 	def valid_move?(dest_x, dest_y)
+		if off_the_board?(dest_x,dest_y)
+			raise "invalid input"
+		elsif !L_move?(dest_x,dest_y)
+			raise "invalid input"
+		else
+			return true
+		end
+	end
 
+	private
+
+	def L_move?(dest_x,dest_y)
+		(dest_x - x_position).abs < 3  && (dest_y - y_position).abs < 3  && ((dest_x - x_position).abs + (dest_y - y_position).abs) < 4
 	end
 end
