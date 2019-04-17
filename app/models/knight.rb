cf36078a89493c66e2859c8e8e974a7f20d34f5c
Kevins MBP <kevlau68@gmail.com>
2019-04-17 02:07:34 +0000

    Added check for occupied by friendly piece

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -3,6 +3,8 @@ class Knight < Piece
 	def valid_move?(dest_x, dest_y)
 		if off_the_board?(dest_x,dest_y)
 			raise "invalid input"
+		elsif is_occupied_by_friend?(dest_x,dest_y)
+			raise "invalid input"
 		elsif !L_move?(dest_x,dest_y)
 			raise "invalid input"
 		else
