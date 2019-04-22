adeca1125dcea008e37b0b250908998719e039b6
Kevins MBP <kevlau68@gmail.com>
2019-04-22 01:53:33 +0000

    add adjustments to unify the move and capture functionality

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,41 +1,25 @@
 class Piece < ApplicationRecord
 	belongs_to :game
 
-	# This function assumes the move is valid
-	def move_to(new_x, new_y)
-
-		game.pieces.each do |piece|
-			# Check if the new_position is occupied
-			if piece.y_position == new_y and piece.x_position == new_x begin
-				# Capture the piece  
-				if piece.color != color begin
-					piece.destroy
-					break
-				end else begin
-					raise "Can't capture your own mate"
-					return	
-				end
-			end
-		end
-		x_position = new_x;
-		y_position = new_y;
-	end
-
 	def lat_check(dest_x,dest_y)																						#Checks if a lateral move is obstructed
 		game.pieces.each do |piece|																						#Loops across all pieces in the board to see if a piece is in between the start location and the destination
 			if piece.y_position == y_position && (piece.x_position.between?(dest_x, x_position) or piece.x_position.between?(x_position, dest_x)) && piece.x_position != x_position
+				puts "move passed lateral check"
 				return true
 			end
 		end
+		puts "failed lat check"
 		return false																													#returns false if unobstructed
 	end
 
 	def long_check(dest_x,dest_y)
 		game.pieces.each do |piece|																						#Checks if a vertical move is obstructed using the same technique as lateral moves
 			if piece.x_position == x_position && (piece.y_position.between?(dest_y, y_position) or piece.y_position.between?(y_position, dest_y)) && piece.y_position != y_position
+				puts "verticle move is obstructed"
 				return true
 			end
 		end
+		puts "verticle move not obstructed"
 		return false
 	end
 
@@ -54,37 +38,40 @@ class Piece < ApplicationRecord
 	end
 
 	def off_the_board?(dest_x,dest_y)																				#check if move is off the board
-		if dest_x > 7 || dest_y > 7 || dest_x < 0 || dest_y < 0 	
+		if dest_x.to_i > 7 || dest_y.to_i > 7 || dest_x.to_i < 0 || dest_y.to_i < 0 	
 			return true
 		end
 	end
 
 	def is_lateral?(dest_x,dest_y)
-		if y_position == dest_y																								#check if the move is lateral
+		if y_position.to_i == dest_y.to_i																								#check if the move is lateral
 			return true
 		end
 	end
 
 	def is_vertical?(dest_x,dest_y)																					#check if the move is vertical
-		if x_position == dest_x
+		if x_position.to_i == dest_x.to_i
 			return true
 		end
 	end
 
 	def is_diagonal?(dest_x,dest_y)
-		if ((y_position - dest_y)/(x_position - dest_x)).abs.eql?(1)					#check if move is diagonal
+		if ((y_position.to_i - dest_y.to_i)/(x_position.to_i - dest_x.to_i)).abs.eql?(1)					#check if move is diagonal
 			return true
 		end
 	end
 
 	def is_obstructed?(dest_x,dest_y)
 		if off_the_board?(dest_x,dest_y)							
-			raise "invalid input"
+			raise "invalid input: Piece is off the board!"
 		elsif is_lateral?(dest_x,dest_y)
+			puts "move is lateral"
 			return lat_check(dest_x,dest_y)
 		elsif is_vertical?(dest_x,dest_y)
+			puts "move is vertical"
 			return long_check(dest_x,dest_y)
 		elsif is_diagonal?(dest_x,dest_y)
+			puts "move is diagonal"
 			return diag_check(dest_x,dest_y)																		#check if the move is diagonal
 		else																																	#returns error "invalid input" if the move is not diagonal, vertical, or lateral
 			raise "invalid input"
@@ -92,18 +79,35 @@ class Piece < ApplicationRecord
 	end
 
 	def is_occupied_by_friend?(dest_x,dest_y)
-		game.pieces.where(x_position: dest_x, y_position: dest_y, color: color).exists?
+		puts "space is occupied by friend"
+		game.pieces.where(x_position: dest_x.to_i, y_position: dest_y.to_i, color: color).exists?
 	end
 
 	def is_occupied_by_enemy?(dest_x,dest_y)
-		game.pieces.where(x_position: dest_x, y_position: dest_y).not(color: color).exists?
+		puts "space is occupied by enemy"
+		game.pieces.where(x_position: dest_x.to_i, y_position: dest_y.to_i).exists? && game.pieces.where(x_position: dest_x.to_i, y_position: dest_y.to_i).color != color
 	end
 
 	def move_to!(dest_x,dest_y)
 		if valid_move?(dest_x, dest_y)
+			puts "move is valid"
+				if is_occupied_by_enemy?(dest_x,dest_y)
+					puts "space is occupied by enemy"
+					defending_piece = game.pieces.where(x_position: dest_x, y_position: dest_y).first
+					# Check if the new_position is occupied
+					if defending_piece.y_position.to_i == dest_y.to_i && defending_piece.x_position.to_i == dest_x.to_i
+						# Capture the piece
+						if color != defending_piece.color
+							defending_piece.destroy
+						else
+							raise "Can't capture your own mate"
+						end
+					end
+				else
+				end
 			update_attributes(x_position: dest_x,y_position: dest_y)
 		else
-			raise "invalid input"
+			raise "invalid input when trying to move piece"
 		end
 	end
 
--- app/models/rook.rb
+++ app/models/rook.rb
@@ -3,15 +3,16 @@
 class Rook < Piece
   def valid_move?(dest_x, dest_y)
     if is_obstructed?(dest_x, dest_y)
-      false
+      return false
     elsif is_occupied_by_friend?(dest_x, dest_y)
-      false
+      return false
     elsif is_lateral?(dest_x, dest_y)
-      true
+      return true
     elsif is_vertical?(dest_x, dest_y)
-      true
+      return true
     else
-      false
+      puts "move is invalid"
+      return false
     end
   end
 end
