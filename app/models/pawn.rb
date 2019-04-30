120e14995dcaabb841794aaef30c8e98ee0064ec
Kevins MBP <kevlau68@gmail.com>
2019-04-30 01:40:03 +0000

    added valid move check for pawn

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,2 +1,21 @@
 class Pawn < Piece
+	def valid_move?(dest_x, dest_y)
+		if off_the_board?(dest_x,dest_y)
+			raise "invalid input"
+		elsif is_vertical?(dest_x,dest_y) && (dest_y == y_position + 1) && !is_occupied_by_friend?(dest_x, dest_y)
+			return true
+		elsif first_move?(:y_position) &&  (dest_y == y_position + 2)
+			return true
+		elsif is_diagonal?(dest_x,dest_y) && (dest_x - x_position) == 1 && !is_occupied_by_friend?(dest_x, dest_y)
+		 	return true
+		else
+			raise "invalid input"
+		end
+	end
+
+	private
+
+	def first_move?(y_position)
+		(y_position == 1) || (y_position == 6)
+	end
 end
